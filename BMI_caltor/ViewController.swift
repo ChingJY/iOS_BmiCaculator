960a26fdcd5a65dd8392b74b262cd7fe831693a8
Ching <>
2018-12-03 21:01:06 +0800

    first init

--- BMI_caltor/ViewController.swift
+++ BMI_caltor/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var heightTf: UITextField!
+    @IBOutlet weak var weightTf: UITextField!
+    @IBOutlet weak var bmiLb: UILabel!
+    @IBOutlet weak var resultLb: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func sentBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func resultBtn(_ sender: UIButton) {
+    }
+    
 }
 
